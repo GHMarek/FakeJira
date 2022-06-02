7613d52c76553ed8b95a59a1025fbeb8822a2baf
GHMarek <47536098+GHMarek@users.noreply.github.com>
2022-06-02 18:19:15 +0200

    Added database and class library.

---
+++ FakeJira/FakeJiraDB/dbo/tables/Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity
+	, FirstName nvarchar(250)
+	, LastName nvarchar(250)
+	, EmailAddress nvarchar(250)
+	, DepartmentId int
+	, BusinessRoleId int
+)
